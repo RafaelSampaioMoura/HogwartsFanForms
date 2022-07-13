0b0621fb291c7ca24a692664b30dedf5c34f6c84
HailMuffins <rafaelsm123@outlook.com>
2022-07-13 16:01:45 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,16 @@
+const loginEmail = document.querySelector("#login-email");
+const loginSenha = document.querySelector("#login-password");
+const loginBtn = document.querySelector("#login-btn");
+
+// console.log(loginBtn);
+
+loginBtn.addEventListener("click", () => {
+  const userEmail = loginEmail.value;
+  const userSenha = loginSenha.value;
+
+  if (userEmail === "tryber@teste.com" && userSenha === "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+});
