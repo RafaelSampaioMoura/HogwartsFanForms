7ce8cc94d2971625692de063a82c72456a77de07
HailMuffins <rafaelsm123@outlook.com>
2022-07-14 14:36:50 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const agreementBox = document.querySelector('#agreement');
 const submitBtn = document.querySelector('#submit-btn');
 const charCounter = document.querySelector('#counter');
 const comentario = document.querySelector('#textarea');
+const evaluationForm = document.querySelector('#evaluation-form');
+const trybewartsFormLogo = document.querySelector('#trybewarts-forms-logo');
 
 // console.log(loginBtn);
 
@@ -20,7 +22,7 @@ loginBtn.addEventListener('click', () => {
 });
 
 agreementBox.addEventListener('change', () => {
-  if(agreementBox.checked === false) {
+  if (agreementBox.checked === false) {
     submitBtn.disabled = true;
   } else {
     submitBtn.disabled = false;
@@ -29,4 +31,60 @@ agreementBox.addEventListener('change', () => {
 
 comentario.addEventListener('input', () => {
   charCounter.textContent = 500 - comentario.value.length;
-})
+});
+
+function pegaInfo() {
+  const usuario = [];
+  usuario.push(document.querySelector('#input-name').value);
+  usuario.push(document.querySelector('#input-lastname').value);
+  usuario.push(document.querySelector('#input-email').value);
+  usuario.push(document.querySelector('#house').value);
+  usuario.push(document.querySelector('input[name="family"]:checked').value);
+  return usuario;
+}
+
+function nomeCompleto() {
+  const informações = pegaInfo();
+  const nomePerfil = document.createElement('li');
+  nomePerfil.textContent = `Nome: ${informações[0]} ${informações[1]}`;
+  return nomePerfil;
+}
+
+function emailPerfil() {
+  const informações = pegaInfo();
+  const email = document.createElement('li');
+  email.textContent = `Email: ${informações[2]}`;
+  return email;
+}
+
+function casaPerfil() {
+  const informações = pegaInfo();
+  const house = document.createElement('li');
+  house.textContent = `Casa: ${informações[3]}`;
+  return house;
+}
+
+function familiaPerfil() {
+  const informações = pegaInfo();
+  const family = document.createElement('li');
+  family.textContent = `Família: ${informações[4]}`;
+  return family;
+}
+
+function montaPerfil() {
+  const perfil = document.createElement('ul');
+
+  perfil.appendChild(nomeCompleto());
+  perfil.appendChild(emailPerfil());
+  perfil.appendChild(casaPerfil());
+  perfil.appendChild(familiaPerfil());
+  perfil.id = 'perfil-aluno';
+
+  document.querySelector('main').insertBefore(perfil, trybewartsFormLogo);
+}
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  evaluationForm.style.display = 'none';
+  montaPerfil();
+});
